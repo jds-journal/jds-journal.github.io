d2e9c2b8b8f151dd87d493847e9f4ffb70c5b66f
John Lafferty <lafferty@gmail.com>
2023-01-28 16:12:25 -0600

    added rows at bottom

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ let colors = ["#EAEBE4", "#008D4E", "#FFFFFF"]
 
 
 function randomColor() {
-	return colors[Math.floor(Math.random()*colors.length)]
+	return colors[Math.floor(Math.random() * colors.length)]
 }
 
 function renderGraph(element) {
@@ -11,32 +11,33 @@ function renderGraph(element) {
 	const xmax = Math.floor(container.width)
 	const ymax = Math.floor(container.height)
 
-	const maxCircleSize = 25
+	const maxCircleSize = 30
+	const eps = 0.3
 
 	const xnum = Math.ceil(xmax / maxCircleSize)
 	const ynum = Math.ceil(ymax / maxCircleSize)
-	const xSize = xmax/xnum
-	const ySize = ymax/ynum
+	const xSize = xmax / xnum
+	const ySize = ymax / ynum
 
 	document.getElementById("image-main").innerHTML = "";
-	
-	for (let i=0; i<xnum; i++) {
-		for (let j=0; j<ynum; j++) {
+
+	for (let i = 0; i < xnum; i++) {
+		for (let j = 0; j < ynum; j++) {
 			if (Math.random() < 0) {
-				const coords = [[i*xSize,j*ySize],[i*xSize,j*ySize+ySize], [i*xSize+xSize,j*ySize]]
-				drawPolygon(coords,randomColor(), "image-main")			
+				const coords = [[i * xSize, j * ySize], [i * xSize, j * ySize + ySize], [i * xSize + xSize, j * ySize]]
+				drawPolygon(coords, randomColor(), "image-main")
 			} else {
-				drawCircle([xSize/2+i*xSize,ySize/2+j*ySize], Math.min(xSize,ySize)/2*Math.random(), randomColor(), "image-main")			
+				drawCircle([xSize / 2 + i * xSize, ySize / 2 + j * ySize], Math.min(xSize, ySize) / 2 * ((1-eps)*Math.random()+eps), randomColor(), "image-main")
 			}
 		}
-	}  	
+	}
 }
 
-document.addEventListener('DOMContentLoaded', function () {	
+document.addEventListener('DOMContentLoaded', function () {
 
 	if (document.getElementById("image-main")) {
 		renderGraph(document.getElementById("image-main"));
-		window.addEventListener('resize', function(event) {
+		window.addEventListener('resize', function (event) {
 			renderGraph(document.getElementById("image-main"));
 		}, true);
 	}
