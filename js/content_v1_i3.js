dcdf4f28f04b91b01c61f749c3fa8717c81f079d
Utku Sirin <utkusirin@Utkus-MBP.cable.rcn.com>
2023-12-23 17:25:59 -0500

    issue3 step3.

---
+++ js/content_v1_i3.js
@@ -0,0 +1,37 @@
+const issues = {
+	"Volume 1 Issue 3" : [
+		{
+			"cover_url": "./files/JDS_Issue2_Cover1.pdf",
+			"paper_url": "./files/JDS_Issue2_Paper1.pdf",
+			"title": "Identification and semiparametric efficiency theory of nonignorable missing data with a shadow variable",
+			"abstract": "We consider identification and estimation with an outcome missing not at random (MNAR). We study an identification strategy based on a so-called shadow variable. A shadow variable is assumed to be correlated with the outcome, but independent of the missingness process conditional on the outcome and fully observed covariates. We describe a general condition for nonparametric identification of the full data law under MNAR using a valid shadow variable. Our condition is satisfied by many commonly- used models; moreover, it is imposed on the complete cases, and therefore has testable implications with observed data only. We characterize the semiparametric efficiency bound for the class of regular and asymptotically linear estimators, and derive a closed form for the efficient influence function. We describe a doubly robust and locally efficient estimation method and evaluate its performance on both simulation data and a real data example about home pricing.",
+			"authors": [
+				{
+					"name": "Wang Miao",
+					"affiliation": "Peking University",
+					"email": "mwfy@pku.edu.cn"
+				},
+				{
+					"name": "Lan Liu",
+					"affiliation": "University of Minnesota",
+					"email": "liux3771@umn.edu"
+				},
+				{
+					"name": "Yilin Li",
+					"affiliation": "Peking University",
+					"email": "yilinli@pku.edu.cn"
+				},
+				{
+					"name": "Eric Tchetgen Tchetgen",
+					"affiliation": "University of Pennsylvania",
+					"email": "ett@wharton.upenn.edu"
+				},
+				{
+					"name": "Zhi Geng",
+					"affiliation": "Peking University",
+					"email": "zhigeng@pku.edu.cn"
+				}
+			]
+		}
+	]
+}
